56fd911f6f291e92e0c342e7f879b41c6cd84dbb
nitin-das <nitin.das@thomson.co.uk>
2020-02-14 22:40:03 +0000

    Modified code

--- variables.tf
+++ variables.tf
@@ -1,5 +1,73 @@
-# ## Tags
-# variable "tags" {
-#   description = "Resources Tags"
-#   type        = map(string)
-# }
+## SQS Queue
+
+variable "name" {
+  description = "This is the human-readable name of the queue"
+}
+
+variable "visibility_timeout_seconds" {
+  description = "The visibility timeout for the queue"
+  type        = number
+  default     = 30
+}
+
+variable "message_retention_seconds" {
+  description = "The number of seconds Amazon SQS retains a message"
+  type        = number
+  default     = 345600
+}
+
+variable "max_message_size" {
+  description = "The limit of how many bytes a message can contain before Amazon SQS rejects it."
+  type        = number
+  default     = 345600
+}
+
+variable "delay_seconds" {
+  description = "The time in seconds that the delivery of all messages in the queue will be delayed"
+  type        = number
+  default     = 0
+}
+
+variable "receive_wait_time_seconds" {
+  description = "The time for which a ReceiveMessage call will wait for a message to arrive (long polling) before returning."
+  type        = number
+  default     = 0
+}
+
+variable "policy" {
+  description = "The JSON policy for the SQS queue."
+  default     = null
+}
+
+variable "redrive_policy" {
+  description = "The JSON policy to set up the Dead Letter Queue"
+  default     = null
+}
+
+variable "fifo_queue" {
+  description = "description"
+  type        = bool
+  default     = false
+}
+
+variable "content_based_deduplication" {
+  description = "Enables content-based deduplication for FIFO queues"
+  default     = null
+}
+
+variable "kms_master_key_id" {
+  description = "The ID of an AWS-managed customer master key (CMK) for Amazon SQS or a custom CMK"
+  default     = null
+}
+
+variable "kms_data_key_reuse_period_seconds" {
+  description = "The length of time, in seconds, for which Amazon SQS can reuse a data key"
+  type        = number
+  default     = 300
+}
+
+## Tags
+variable "tags" {
+  description = "Resources Tags"
+  type        = map(string)
+}
